4d2072e2e6d0352f4062730271302bbe1f10ada6
Adam Zivlak <Adam.zivlak99@hotmail.se>
2023-03-22 15:46:02 +0100

    Test

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactList_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("test");
+        }
+    }
+}
